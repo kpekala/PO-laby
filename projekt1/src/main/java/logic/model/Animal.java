2b4968c616f92596ccacf8d71dbcd14b08584d02
kpekala <kgpekala@gmail.com>
2020-12-12 00:28:52 +0100

    projekt1: added animal file

---
+++ projekt1/src/main/java/logic/model/Animal.java
@@ -0,0 +1,92 @@
+package logic.model;
+
+
+
+
+import logic.map.WorldMap;
+
+import java.util.ArrayList;
+import java.util.Objects;
+
+public class Animal implements IMapElement {
+
+    private MapDirection mapDirection;
+    private Vector2d position;
+    private WorldMap map;
+
+    public Animal(){
+        //Teraz niebezpiecznie jest go używać, co z obiektem map?
+        this.mapDirection = MapDirection.NORTH;
+        this.position = new Vector2d(2,2);
+    }
+
+    public Animal(WorldMap map){
+        this.map = map;
+        this.mapDirection = MapDirection.NORTH;
+        this.position = new Vector2d(2,2);
+    }
+
+    public Animal(WorldMap map, Vector2d initialPosition){
+        this.map = map;
+        this.mapDirection = MapDirection.NORTH;
+        this.position = initialPosition;
+    }
+    public void move(MoveDirection direction){
+        switch (direction){
+            case RIGHT -> this.mapDirection = this.mapDirection.next();
+            case LEFT -> this.mapDirection = this.mapDirection.previous();
+            default -> {
+                Vector2d moveVector = this.mapDirection.toUnitVector();
+                Vector2d newPosition;
+                if(direction == MoveDirection.FORWARD)
+                    newPosition = this.position.add(moveVector);
+                else
+                    newPosition = this.position.subtract(moveVector);
+                if(map.canMoveTo(newPosition)){
+                    Vector2d oldPosition = this.position;
+                    this.position = newPosition;
+                    positionChanged(oldPosition, newPosition);
+                }
+            }
+        }
+
+    }
+
+    private void  positionChanged(Vector2d oldPosition, Vector2d newPosition){
+        for (IPositionChangeObserver observer: observers)
+            observer.positionChanged(oldPosition, newPosition);
+    }
+
+    public void addObserver(IPositionChangeObserver observer){
+        this.observers.add(observer);
+    }
+
+    @Override
+    public String toString(){
+        return mapDirection.code();
+    }
+
+    @Override
+    public Vector2d getPosition() {
+        return position;
+    }
+
+    public MapDirection getMapDirection() {
+        return mapDirection;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Animal animal = (Animal) o;
+        return mapDirection == animal.mapDirection &&
+                position.equals(animal.position) &&
+                map.equals(animal.map);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(mapDirection, position, map);
+    }
+}
---
+++ projekt1/src/main/java/logic/model/IMapElement.java
@@ -0,0 +1,5 @@
+package logic.model;
+
+public interface IMapElement {
+    Vector2d getPosition();
+}
